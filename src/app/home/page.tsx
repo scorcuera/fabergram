6c7249a5dcdb99f3b9dd5e7c11e51b97af26fb7a
scorcuera <scorcuera6@gmail.com>
2024-03-26 12:49:03 +0100

    Add react-typed dependency and display idea using ReactTyped

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 
+import { useState } from "react";
 import { createIdea } from "@/app/lib/actions";
 import { motion } from "framer-motion";
+import { ReactTyped } from "react-typed";
 import Form from "../ui/form";
 
 export default function Home() {
+  const [idea, setIdea] = useState("");
+
   const FormAction = async (formData: any) => {
-    const data = await createIdea(formData);
-    console.log(data)
+    const data = await createIdea(formData) as string;
+    setIdea(data);
   }
 
   return (
@@ -16,6 +20,9 @@ export default function Home() {
         <motion.h3 initial={{opacity: 0}} animate={{opacity: 100}} transition={{duration: 0.2}} className="pb-20 text-3xl md:text-5xl">Let's create some ideas</motion.h3>
         <Form handleSubmit={FormAction} />
       </div>
+      <p>
+        <ReactTyped strings={[idea]} typeSpeed={50} />
+      </p>
       <motion.h1 initial={{opacity: 0}} animate={{opacity: 0.3}} transition={{delay: 1, duration: 1}} className="self-center text-3xl font-extralight">Fabergram</motion.h1>
     </main>
   );
