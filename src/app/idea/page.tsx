3950c4fc9f7a045333a76849c99219c99bf3c1e6
scorcuera <scorcuera6@gmail.com>
2024-04-04 07:53:44 +0200

    Add localStorage functionality

--- src/app/idea/page.tsx
+++ src/app/idea/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+
+import { ReactTyped } from "react-typed";
+
 export default function Idea() {
+    const idea = JSON.parse(localStorage.getItem("idea") as string);
+
     return (
         <>
-            <h1>your idea is returned here</h1>
+            <p>
+                <ReactTyped strings={[idea]} typeSpeed={50} />
+            </p>
             <footer>
                 <h1 className="opacity-30 self-center text-3xl font-extralight">Fabergram</h1>
             </footer>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 'use client'
 
-import { useState } from "react";
 import { createIdea } from "@/app/lib/actions";
 import { motion } from "framer-motion";
-import { ReactTyped } from "react-typed";
+import { useRouter } from "next/navigation";
 import Form from "./ui/form";
 
 export default function Home() {
-  const [idea, setIdea] = useState("");
+  const router = useRouter();
 
   const FormAction = async (formData: any) => {
     const data = await createIdea(formData) as string;
-    setIdea(data);
+    if (data) {
+      localStorage.setItem("idea", JSON.stringify(data));
+      router.push("/idea");
+    }
   }
 
   return (
@@ -20,9 +22,6 @@ export default function Home() {
         <motion.h3 initial={{ opacity: 0 }} animate={{ opacity: 100 }} transition={{ duration: 0.2 }} className="pb-20 text-3xl md:text-5xl">Let's create some ideas</motion.h3>
         <Form handleSubmit={FormAction} />
       </div>
-      <p>
-        <ReactTyped strings={[idea]} typeSpeed={50} />
-      </p>
       <footer>
         <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 0.3 }} transition={{ delay: 1, duration: 1 }} className="self-center text-3xl font-extralight">Fabergram</motion.h1>
       </footer>
