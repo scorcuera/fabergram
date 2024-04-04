58bcbaa3f9333c6932254836a9b3c80e22e5c6a3
scorcuera <scorcuera6@gmail.com>
2024-04-04 07:57:13 +0200

    Add ReactTyped component to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { createIdea } from "@/app/lib/actions";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
+import { ReactTyped } from "react-typed";
 import Form from "@/app/ui/form";
 
 export default function Home() {
@@ -22,6 +23,9 @@ export default function Home() {
         <motion.h3 initial={{ opacity: 0 }} animate={{ opacity: 100 }} transition={{ duration: 0.2 }} className="pb-20 text-3xl md:text-5xl">Let's create some ideas</motion.h3>
         <Form handleSubmit={FormAction} />
       </div>
+      <p>
+        <ReactTyped strings={[""]} />
+      </p>
       <footer>
         <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 0.3 }} transition={{ delay: 1, duration: 1 }} className="self-center text-3xl font-extralight">Fabergram</motion.h1>
       </footer>
