68562f5263a7e488ee6647ea5f9a6d7041d9cb0b
scorcuera <scorcuera6@gmail.com>
2024-04-17 18:14:31 +0200

    Update package-lock.json and package.json with new versions of dependencies

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -2,9 +2,7 @@
 
 import OpenAI from "openai";
 
-const openai = new OpenAI(
-  { apiKey: process.env.OPENAI_API_SECRET }
-);
+const openai = new OpenAI({apiKey: process.env.API_KEY });
 
 export const createIdea = async (formData: any) => {
   const firstIdea = formData.get("firstIdea");
@@ -12,10 +10,13 @@ export const createIdea = async (formData: any) => {
   const completion = await openai.chat.completions.create({
     messages: [
       { role: "system", content: "You are a creativity expert" },
-      { role: "user", content: `Mix these ideas to create a brand new one: ${firstIdea} and ${secondIdea}` },
+      {
+        role: "user",
+        content: `Mix these ideas to create a brand new one: ${firstIdea} and ${secondIdea}`,
+      },
     ],
     model: "gpt-3.5-turbo",
   });
   let response = completion.choices[0].message.content;
   return response;
-}
+};
